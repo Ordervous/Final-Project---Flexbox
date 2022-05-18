afd7c0fa86aded84973ddbc0fb01a5de31a1e666
Brett <bdmdiving@gmail.com>
2022-05-18 15:52:42 +1000

    background color changes

--- script.js
+++ script.js
@@ -10,10 +10,7 @@ $.ajax('https://api.adviceslip.com/advice',
             myadvice.innerHTML = justadvice.slip.advice;
 
         }
-    })
-
-
-// function resize(elem, percent) { elem.style.fontSize = percent; };
+    });
 
 document.getElementById("mousehover").onmouseover = function () {
     document.getElementById("mousehover").style.fontSize = "210%"
@@ -26,10 +23,38 @@ document.getElementById("mousehover2").onmouseover = function () {
     document.getElementById("mousehover2").style.fontSize = "180%"
 };
 document.getElementById("mousehover2").onmouseout = function () {
-    document.getElementById("mousehover2").style.fontSize = "150%";
+    document.getElementById("mousehover2").style.fontSize = "135%";
+};
+
+//Technical Skills 
+document.getElementById("orange").onmouseover = function () {
+    document.getElementById("orange").style.backgroundColor = "bisque"
+};
+    document.getElementById("orange").onmouseout = function () {
+        document.getElementById("orange").style.backgroundColor = "";
+};
+
+//Soft Skills 
+document.getElementById("red").onmouseover = function () {
+    document.getElementById("red").style.backgroundColor = "bisque"
+};
+    document.getElementById("red").onmouseout = function () {
+        document.getElementById("red").style.backgroundColor = "";
+};
+
+//Hobbies
+document.getElementById("yellow").onmouseover = function () {
+    document.getElementById("yellow").style.backgroundColor = "bisque"
+};
+    document.getElementById("yellow").onmouseout = function () {
+        document.getElementById("yellow").style.backgroundColor = "";
 };
 
 
+// Was trying to make one function that would change the background 
+//for multiple classes, then id's but couldn't crack it 
 
-/* onmouseover="resize(this, '210%')" onmouseout="resize(this, '190%')" */
-/* onmouseover="resize(this, '180%')" onmouseout="resize(this, '150%')" */
+// function justOne() {
+//     document.getElementsByClassName("background").style.backgroundColor = "bisque"
+  
+// };
